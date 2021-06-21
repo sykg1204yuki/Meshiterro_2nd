75058c407716dca9926593d9d2aaea313cfd28af
sykg1204yuki <y.yuki040507@gamil.com>
2021-06-21 05:38:51 +0000

    画像投稿機能まで

---
+++ Meshiterro/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'c04e0625310575179a829d0bd37fd0195a5457b4554014ef6b8fabab3717184787b012d466652815d11ab83a73fe7ce086b997fac2e106416ec2c40140edfb62'
